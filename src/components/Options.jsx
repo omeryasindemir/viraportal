e34fc692c6d9f988d76018b3742daeac6d0c3cff
omeryasindemir <omeryasindemir66@gmail.com>
2024-11-12 14:33:03 +0300

    opts_and_profile_part_1

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,21 +1,91 @@
 import React from 'react'
 import Icon from "../assets/v.png"
+import { useNavigate } from 'react-router-dom'
 
 const Options = () => {
+
+  const navigate = useNavigate()
+
   return (
     <div className='oBox' style={{
-      minWidth: 320
+      minWidth: 320,
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between"
     }}>
-      <div style={{
-        display: "flex",
-        justifyContent: "center",
-        marginTop: 48
-      }}>
-        <img style={{
-          height: 128,
-          width: 128
-        }} src={Icon} alt="" />
+      <div>
+        <div style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: 48
+        }}>
+          <img style={{
+            height: 128,
+            width: 128
+          }} src={Icon} alt="" />
+        </div>
+
+        <div style={{ marginTop: 48, paddingLeft: 48, paddingRight: 48 }}>
+
+          <div onClick={() => navigate("/home/home")} className='optsOpt click'>
+            <div style={{ display: "flex", gap: 16 }}>
+              <i class="bi bi-house-door"></i>
+              <div>Ana Sayfa</div>
+            </div>
+            <i class="bi bi-caret-right"></i>
+          </div>
+
+          <div onClick={() => navigate("/home/profile")} style={{ marginTop: 32 }} className='optsOpt click'>
+            <div style={{ display: "flex", gap: 16 }}>
+              <i class="bi bi-person"></i>
+              <div>Profil</div>
+            </div>
+            <i class="bi bi-caret-right"></i>
+          </div>
+
+          <div onClick={() => navigate("/home/chat")} style={{ marginTop: 32 }} className='optsOpt click'>
+            <div style={{ display: "flex", gap: 16 }}>
+              <i class="bi bi-chat"></i>
+              <div>Sohbet</div>
+            </div>
+            <i class="bi bi-caret-right"></i>
+          </div>
+
+          <div onClick={() => navigate("/home/apps")} style={{ marginTop: 32 }} className='optsOpt click'>
+            <div style={{ display: "flex", gap: 16 }}>
+              <i class="bi bi-controller"></i>
+              <div>Uygulamalar</div>
+            </div>
+            <i class="bi bi-caret-right"></i>
+          </div>
+
+          <div onClick={() => navigate("/home/wiki")} style={{ marginTop: 32 }} className='optsOpt click'>
+            <div style={{ display: "flex", gap: 16 }}>
+              <i class="bi bi-info-circle"></i>
+              <div>Wiki</div>
+            </div>
+            <i class="bi bi-caret-right"></i>
+          </div>
+
+          <div onClick={() => navigate("/home/offer")} style={{ marginTop: 32 }} className='optsOpt click'>
+            <div style={{ display: "flex", gap: 16 }}>
+              <i class="bi bi-pen"></i>
+              <div>Öneriler</div>
+            </div>
+            <i class="bi bi-caret-right"></i>
+          </div>
+
+        </div>
       </div>
+
+      <div style={{
+        padding: 16
+      }}>
+        <button style={{
+          background: "linear-gradient(to right, var(--light-red), var(--red))"
+        }} className='Btn click'>Çıkış Yap</button>
+      </div>
+
     </div>
   )
 }
--- src/routes/Home/Profile.jsx
+++ src/routes/Home/Profile.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import Crown from "../../assets/crown.png"
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div>
+
+      <div style={{ display: "flex", gap: 32, paddingLeft: 83, paddingTop: 48 }}>
+
+        <div style={{ position: "relative" }}>
+          <div className='pp_rank_info_box'>
+            <img className='click pp_crown' style={{
+              height: 32,
+              position: "absolute",
+              left: 32,
+              top: 12
+            }} src={Crown} alt="" />
+            {/* <div className='rank_info'>Yabancı</div> */}
+          </div>
+          <div style={{
+            height: 128,
+            width: 128,
+            overflow: "hidden",
+            borderRadius: "50%"
+          }}>
+            <img style={{ height: 128, width: 128 }} src={"https://i.pinimg.com/236x/d4/06/6d/d4066df9414e37e47739a84418971f36.jpg"} alt="" />
+          </div>
+        </div>
+
+        <div>
+          <div className='midium_text'>viraportal</div>
+        </div>
+
+      </div>
+
+      <div>profile bottom</div>
+
+    </div>
   )
 }
 
