5d00ef6025a3b1dcd452a12ff794902850a08707
omeryasindemir <omeryasindemir66@gmail.com>
2024-11-13 18:27:44 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Login from "./routes/Auth/Login"
 import Profile from "./routes/Home/Profile"
@@ -9,8 +9,12 @@ import Register from './routes/Auth/Register'
 import baseUrl from './server/env'
 import { authMe } from './server/req/auth'
 import Loading from './components/Loading'
+import Home from "./routes/Home/Home"
 
 const App = () => {
+
+
+  const [isAuth, setisAuth] = useState(false)
 
 
   useEffect(() => {
@@ -19,49 +23,89 @@ const App = () => {
         const data = await authMe()
         console.log(data)
         console.log("Auth Success!")
+
+        setisAuth(true)
+
       } catch (error) {
         console.log("Auth Error!")
       }
     }
 
     tokenControl()
-  },[])
+  }, [])
 
 
   return (
     <Routes>
 
-      <Route path='' element={
-        <div className="auth_body">
-          <Login />
-        </div>
-      } />
-
-      <Route path='/auth/login' element={
-        <div className="auth_body">
-          <Login />
-        </div>
-      } />
-
-      <Route path='/auth/register' element={
-        <div className="auth_body">
-          <Register />
-        </div>
-      } />
+      {
+        !isAuth && <Route path='' element={
+          <div className="auth_body">
+            <Login />
+          </div>
+        } />
+      }
 
 
+      {
+        isAuth && <Route path='' element={<div style={{
+          display: "flex",
+          padding: 8,
+          minHeight: "100vh",
+          gap: 8
+        }}>
+          <Options />
+          <OptionsMobile />
+          <Home />
+        </div>} />
+      }
 
-      <Route path='/home/profile' element={<div style={{
-        display: "flex",
-        padding: 8,
-        minHeight: "100vh",
-        gap: 8
-      }}>
-        <Options />
-        <OptionsMobile />
-        <Profile />
-      </div>} />
 
+
+      {
+        !isAuth && <Route path='/auth/login' element={
+          <div className="auth_body">
+            <Login />
+          </div>
+        } />
+      }
+
+      {
+        !isAuth && <Route path='/auth/register' element={
+          <div className="auth_body">
+            <Register />
+          </div>
+        } />
+      }
+
+
+
+      {
+        isAuth && <Route path='/home/profile' element={<div style={{
+          display: "flex",
+          padding: 8,
+          minHeight: "100vh",
+          gap: 8
+        }}>
+          <Options />
+          <OptionsMobile />
+          <Profile />
+        </div>} />
+      }
+
+
+      {
+        isAuth && <Route path='/home/home' element={<div style={{
+          display: "flex",
+          padding: 8,
+          minHeight: "100vh",
+          gap: 8
+        }}>
+          <Options />
+          <OptionsMobile />
+          <Home />
+        </div>} />
+      }
 
 
 
