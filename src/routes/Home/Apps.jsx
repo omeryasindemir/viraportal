ad2317c3ed057f870effaf405157d97225833967
omeryasindemir <omeryasindemir66@gmail.com>
2024-11-18 09:23:27 +0300

    fix

--- src/routes/Home/Apps.jsx
+++ src/routes/Home/Apps.jsx
@@ -37,13 +37,13 @@ const Apps = () => {
             </div>
             <div style={{ display: "flex", flexDirection: "column", gap: 32, paddingTop: 32, width: "100%", minHeight: 360, maxHeight: 360, justifyContent: "space-between" }} className="tBox">
                 <div style={{ display: "flex", flexDirection: "column", gap: 32 }}>
-                    <div style={{ textAlign: "center" }}><i style={{ fontSize: 64 }} className="bi bi-wallet2"></i></div>
+                    <div style={{ textAlign: "center" }}><i style={{ fontSize: 64 }} className="bi bi-hammer"></i></div>
                     <div style={{ display: "flex", flexDirection: "column", gap: 8 }}>
-                        <div style={{ textAlign: "center", display: "flex", gap: 16, justifyContent: "center", alignItems: "center" }} className='midium_text'>viracommerce<span style={{ color: "gold" }} className='des_text'>4/5 <i className="bi bi-star-fill"></i></span></div>
-                        <div style={{ textAlign: "center" }} className='des_text'>viracommerce sayesinde mevcut mağazalarınız artık çok daha popüler!</div>
+                        <div style={{ textAlign: "center", display: "flex", gap: 16, justifyContent: "center", alignItems: "center" }} className='midium_text'>MULA<span style={{ color: "gold" }} className='des_text'>4/5 <i className="bi bi-star-fill"></i></span></div>
+                        <div style={{ textAlign: "center" }} className='des_text'>MULA'nın 2.0 sürümü ile ihaleleriniz artık çok daha pratik ve garanti!</div>
                     </div>
                 </div>
-                <div><button className='Btn click'>Kullanmaya Başla</button></div>
+                <div><button className='Btn click'>Talep Et</button></div>
             </div>
             <div style={{ display: "flex", flexDirection: "column", gap: 32, paddingTop: 32, width: "100%", minHeight: 360, maxHeight: 360, justifyContent: "space-between" }} className="tBox">
                 <div style={{ display: "flex", flexDirection: "column", gap: 32 }}>
